a5fc8a491267c235bbf982bdc16c0d2e8402cf94
dajung <kwonkkan@naver.com>
2024-08-13 19:20:22 +0900

    파리 올림픽 메달 트래커

---
+++ paris-olympic-medal-tracker/src/App.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  // 새로운 배열
+  const [countryArr, setCountryArr] = useState([]);
+
+  // 배열에 들어갈 요소들
+  const [country, setCountry] = useState('');
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
+
+  // 📌 추가하기
+  // 주석 먼저 쓰고 코드 짜기
+  // 1. 먼저 있는 배열에 추가로 입력되는 객체 넣기
+  // 2. state 변경^^
+  const handleAddCountry = () => {
+    const 추가 = {
+      name: country,
+      gold,
+      silver,
+      bronze,
+    };
+
+    const 새배열 = [...countryArr, 추가];
+    setCountryArr(새배열);
+  };
+
+  // 📌 업데이트
+  // 국가가 같은 걸 찾아서 메달 개수 수정
+  // find는 조건에 맞는 것 중 제일 먼저 발견한 것만 반환함
+  const handleUpdateCountry = (event) => {
+    event.preventDefault();
+
+    const 비교 = countryArr.find(function (대상) {
+      if (대상.name === country) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    const 수정 = countryArr.map(function (대상) {
+      if (대상.name === 비교.name) {
+        return {
+          ...대상,
+          gold,
+          silver,
+          bronze,
+        };
+      } else {
+        return 대상;
+      }
+    });
+
+    setCountryArr(수정);
+  }; // handleUpdateCountry 끝
+
+  // 📌 삭제하기
+  // 삭제하기 btn이 클릭됐을 때 이 버튼이 어느 국가의 삭제하기 btn인지 판별하기 위해 버튼 자체에서 국가의 이름을 넘겨줘야 함
+  // 그리고 그 넘겨준 인자(국가의 이름)를 여기 버튼 핸들러에서 받아오면 됨
+  // 이 버튼은 form 태그 안에 있는 것이 아니기 때문에 prevent 필요 없음
+  const handleDeleteCountry = ({ name }) => {
+    // const {name} = country;
+    const 삭제 = countryArr.filter(function (대상) {
+      if (대상.name === name) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setCountryArr(삭제);
+  };
+
+  // handleDeleteCountry 끝
+
+  return (
+    <div className="container">
+      <h1>2024 파리 올림픽 메달 트래커</h1>
+      <form
+        className="input-group"
+        onSubmit={function (e) {
+          e.preventDefault(); // 새로고침 방지
+
+          // 갱신할 배열 새롭게 만들어주기
+          const newCountry = [];
+          setCountry(newCountry);
+        }}
+      >
+        {/* 국가 이름, 메달 갯수의 입력 필드와 버튼 위치 */}
+        <div>
+          <h4>국가명</h4>
+          <input
+            type="text"
+            placeholder="국가 입력"
+            value={country}
+            onChange={(e) => {
+              const newCountry = e.target.value;
+              setCountry(newCountry);
+            }}
+          />
+        </div>
+        <div>
+          <h4>금메달</h4>
+          <input
+            type="number"
+            value={gold}
+            onChange={(e) => {
+              const newGold = e.target.value;
+              setGold(newGold);
+            }}
+          />
+        </div>
+        <div>
+          <h4>은메달</h4>
+          <input
+            type="number"
+            value={silver}
+            onChange={(e) => {
+              const newSilver = e.target.value;
+              setSilver(newSilver);
+            }}
+          />
+        </div>
+        <div>
+          <h4>동메달</h4>
+          <input
+            type="number"
+            value={bronze}
+            onChange={(e) => {
+              const newBronze = e.target.value;
+              setBronze(newBronze);
+            }}
+          />
+        </div>
+        <div className="button-group">
+          <button onClick={handleAddCountry} type="submit">
+            국가 추가
+          </button>
+          <button onClick={handleUpdateCountry} type="submit">
+            업데이트
+          </button>
+        </div>
+      </form>
+      <div>
+        {/* 메달 집계 리스트가 표시될 자리 */}
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>국가명</th>
+                <th>금메달</th>
+                <th>은메달</th>
+                <th>동메달</th>
+                <th>액션</th>
+              </tr>
+            </thead>
+            <tbody>
+              {countryArr.map((country) => (
+                <tr key={country.name}>
+                  <td>{country.name}</td>
+                  <td>{country.gold}</td>
+                  <td>{country.silver}</td>
+                  <td>{country.bronze}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteCountry(country)}
+                      type="submit"
+                    >
+                      삭제
+                    </button>
+                    {/* 함수로 감싼다/ 화살표 함수 빼면 함수의 리턴값임*/}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
